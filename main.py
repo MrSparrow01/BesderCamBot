f364c3907ab1fe4b070252a20002e407ba6f4dba
Maks <92633536+MrSparrow01@users.noreply.github.com>
2024-01-29 21:39:26 +0200

    Bot file 2.0
    Add re for detection current minute. This feature help to send alert
    message only one per minute. Other will write to log file.

--- main.py
+++ main.py
@@ -4,38 +4,45 @@ import json
 import asyncio
 from datetime import datetime
 import pytz
+import re
 
 BOT_TOKEN = "6439165432:AAG5L4nuWDadmgVtCInngYzOMBZNtwuYNd8"
 CHAT_ID = "360805436"
 
 bot = telebot.async_telebot.AsyncTeleBot(BOT_TOKEN)
 
+reference_minute = ""
 async def write_to_file(text):
+    kiev_time = pytz.timezone('Europe/Kiev').normalize(datetime.now(tz=pytz.utc)).strftime('%Y-%m-%d %H:%M:%S')
     with open('logs.txt', 'a') as file:
-        file.write(text)
+        file.write(f"[{kiev_time}] {text}\n")
 
 async def send_alarm_message(alarm_data):
+    global reference_minute
     serial_number = alarm_data.get('SerialID')
     alert_event = alarm_data.get('Event')
     alert_time = alarm_data.get('StartTime')
     alert_status = alarm_data.get('Status')
+    current_minute = re.search(r'(\d+):(\d+):(\d+)', alert_time)[2]
     if serial_number == '44098fe28501926a':
         place = 'Door'
     elif serial_number == '81752845777b8188':
         place = "Garage"
     if alert_event == "HumanDetect":
         if alert_status == "Start":
-            await bot.send_message(CHAT_ID,
+            if current_minute != reference_minute:
+                await bot.send_message(CHAT_ID,
                                    f"🚨 Warning! 🚨\nPlace: *{place}*\nEvent: Human Detection\nTime: {alert_time}",
                                    parse_mode='Markdown')
+                reference_minute = current_minute
+            else:
+                await write_to_file(f"Place: {place} - Event: Human Detection")
     else:
-        with open('logs.txt', 'a') as file:
-            file.write(f"[{alarm_data.get('StartTime')}] Place: {place} - Event: {alert_event}\n")
+        await write_to_file(f"Place: {place} - Event: {alert_event}")
 
 async def handle_client(reader, writer):
     addr = writer.get_extra_info('peername')
-    kiev_time = pytz.timezone('Europe/Kiev').normalize(datetime.now(tz=pytz.utc)).strftime('%m-%d-%Y %H:%M:%S')
-    await write_to_file(f"[{kiev_time}] Connection from {addr}\n")
+    await write_to_file(f"Connection from {addr}")
     while True:
         data = await reader.read(1024)
         if not data:
@@ -47,15 +54,14 @@ async def handle_client(reader, writer):
                 alarm_data = json.loads(json_data)
                 await send_alarm_message(alarm_data)
         except Exception as e:
-            await write_to_file(f"[{kiev_time}] {e}\n")
+            await write_to_file(e)
     writer.close()
     await writer.wait_closed()
 
 async def run_server(host='0.0.0.0', port=8080):
     server = await asyncio.start_server(handle_client, host, port)
     addr = server.sockets[0].getsockname()
-    kiev_time = pytz.timezone('Europe/Kiev').normalize(datetime.now(tz=pytz.utc)).strftime('%m-%d-%Y %H:%M:%S')
-    await write_to_file(f"[{kiev_time}] Serving on {addr}\n")
+    await write_to_file(f"Serving on {addr}")
     async with server:
         await server.serve_forever()
 
